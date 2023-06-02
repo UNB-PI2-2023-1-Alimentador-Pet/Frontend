7904047ad3a9f23a06ab891384f8fa7c6d66c10c
Ítalo Alves <italooko@gmail.com>
2023-06-02 17:26:59 -0300

    Adiciona novas telas no AuthStack
    Co-authored-by: Guilherme Fernandes <guilhermedfs11@gmail.com>

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -2,13 +2,28 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Welcome from '../screens/auth/Welcome';
+import Login from '../screens/auth/Login';
+import LostPassword from '../screens/auth/LostPassword';
+import Register from '../screens/auth/Register';
 
 const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
   return (
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={Welcome} />
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{
+        headerBackTitleVisible: false,
+        //headerTransparent: true,
+      }}>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="LostPassword" component={LostPassword} />
     </Stack.Navigator>
   );
 };
