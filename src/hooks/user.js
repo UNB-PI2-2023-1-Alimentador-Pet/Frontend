43963095aee4066ddc701ee94e28cfbca425ec64
Ítalo Alves <italooko@gmail.com>
2023-06-03 16:59:51 -0300

    Adiciona storeUser e signOut no User hook
    Co-authored-by: Guilherme Fernandes <guilhermedfs11@gmail.com>

--- src/hooks/user.js
+++ src/hooks/user.js
@@ -1,17 +1,46 @@
 import React, {createContext, useState, useContext} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const UserContext = createContext(null);
 
 export const UserProvider = ({children}) => {
+  const [user, setUser] = useState({});
+  const [token, setToken] = useState('');
+
   const [isLoading, setIsLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const storeUser = async (user, token = null) => {
+    if (user) {
+      setUser(user);
+      await AsyncStorage.setItem('user', JSON.stringify(user));
+    }
+    if (token) {
+      setToken(token);
+      await AsyncStorage.setItem('token', token);
+    }
+    setIsLoggedIn(true);
+  };
+
+  const signOut = async () => {
+    try {
+      await AsyncStorage.removeItem('user');
+      await AsyncStorage.removeItem('token');
+    } catch (e) {
+      console.log(e);
+    }
+    setIsLoggedIn(false);
+  };
 
   return (
     <UserContext.Provider
       value={{
         isLoading,
         isLoggedIn,
-        setIsLoggedIn,
+        token,
+        user,
+        storeUser,
+        signOut,
       }}>
       {children}
     </UserContext.Provider>
