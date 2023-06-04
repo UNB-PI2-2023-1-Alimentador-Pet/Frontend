8eda2768697a94d106e05831f89207262e17b8a0
Ítalo Alves <italooko@gmail.com>
2023-06-04 14:20:58 -0300

    Cria tela Profile
    Co-authored-by: Guilherme Fernandes <guilhermedfs11@gmail.com>

---
+++ src/screens/app/Profile/index.js
@@ -0,0 +1,91 @@
+import React, {useRef, useState} from 'react';
+import {Alert, Keyboard} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+import {Container} from './styles';
+import {ScrollViewStyled, InputText} from '../../../components/Defaults';
+import {ButtonPrimary, ButtonText} from '../../../components/ButtonPrimary';
+
+import {useUser} from '../../../hooks/user';
+import {updateUser} from '../../../services/user';
+
+const Profile = () => {
+  const {token, user, storeUser, signOut} = useUser();
+
+  const [nome, setNome] = useState(user.nome);
+  const [email, setEmail] = useState(user.email);
+  const [senha, setSenha] = useState('');
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const nomeInput = useRef();
+  const emailInput = useRef();
+  const senhaInput = useRef();
+
+  const handleEdit = async () => {
+    Keyboard.dismiss();
+
+    const newUser = {
+      nome,
+      email,
+      senha: senha || undefined,
+    };
+
+    const response = await updateUser(newUser, user.userHash, token);
+
+    if (response.status === 200) {
+      storeUser({...user, newUser});
+    } else if (response.status === 401) {
+      Alert.alert('Seu token expirou, faça login novamente');
+    } else {
+      Alert.alert('Erro ao editar perfil');
+    }
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Container>
+        <ScrollViewStyled>
+          <InputText
+            ref={nomeInput}
+            placeholder="Nome"
+            maxLength={100}
+            value={nome}
+            onChangeText={text => setNome(text)}
+            onSubmitEditing={() => emailInput.current?.focus()}
+            returnKeyType="next"
+          />
+          <InputText
+            ref={emailInput}
+            placeholder="E-mail"
+            keyboardType="email-address"
+            maxLength={100}
+            value={email}
+            onChangeText={text => setEmail(text)}
+            onSubmitEditing={() => senhaInput.current?.focus()}
+            returnKeyType="next"
+          />
+          <InputText
+            ref={senhaInput}
+            placeholder="Nova Senha"
+            secureTextEntry
+            maxLength={100}
+            value={senha}
+            onChangeText={text => setSenha(text)}
+            returnKeyType="done"
+          />
+
+          <ButtonPrimary onPress={() => handleEdit()}>
+            <ButtonText>Salvar</ButtonText>
+          </ButtonPrimary>
+
+          <ButtonPrimary onPress={() => signOut(false)}>
+            <ButtonText>Sair</ButtonText>
+          </ButtonPrimary>
+        </ScrollViewStyled>
+      </Container>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
---
+++ src/screens/app/Profile/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+import {colorsLight} from '../../../utils/colors';
+import {scale, percentage} from '../../../utils/scalling';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${colorsLight.lightGray};
+`;
