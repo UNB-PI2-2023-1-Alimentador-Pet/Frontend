77845ef26db3821b2cb7f92f29ad40a7c7d0018e
Ítalo Alves <italooko@gmail.com>
2023-06-07 10:03:33 -0300

    Carrega dados persistentes no hook de User

--- src/hooks/user.js
+++ src/hooks/user.js
@@ -1,14 +1,46 @@
-import React, {createContext, useState, useContext} from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const UserContext = createContext(null);
 
 export const UserProvider = ({children}) => {
-  const [user, setUser] = useState({});
   const [token, setToken] = useState('');
+  const [user, setUser] = useState({});
+  const [devices, setDevices] = useState([]);
 
   const [isLoading, setIsLoading] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  useEffect(() => {
+    loadStoredData();
+  }, [loadStoredData]);
+
+  const loadStoredData = useCallback(async () => {
+    // Loads only essencial data stored
+    const storedToken = await AsyncStorage.getItem('token');
+    const storedUser = JSON.parse(await AsyncStorage.getItem('user'));
+    const storedDevices = JSON.parse(await AsyncStorage.getItem('devices'));
+
+    if (storedToken) {
+      setToken(storedToken);
+    } else {
+      await signOut();
+    }
+    if (storedUser) {
+      setUser(storedUser);
+    }
+    if (storedDevices) {
+      setDevices(storedDevices);
+    }
+
+    setIsLoading(false);
+  }, []);
 
   const storeUser = async (user, token = null) => {
     if (user) {
@@ -26,10 +58,18 @@ export const UserProvider = ({children}) => {
     try {
       await AsyncStorage.removeItem('user');
       await AsyncStorage.removeItem('token');
+      await AsyncStorage.removeItem('devices');
     } catch (e) {
       console.log(e);
     }
     setIsLoggedIn(false);
+  };
+
+  const storeDevices = async devices => {
+    if (devices) {
+      setDevices(devices);
+      await AsyncStorage.setItem('devices', JSON.stringify(devices));
+    }
   };
 
   return (
@@ -41,6 +81,8 @@ export const UserProvider = ({children}) => {
         user,
         storeUser,
         signOut,
+        devices,
+        storeDevices,
       }}>
       {children}
     </UserContext.Provider>
